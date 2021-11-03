ae232b016da92cb622f64daabf490740bb068463
Кирпинёв Игорь Валерьевич <IKirpinev@vimpelcom.ru>
2021-11-03 21:50:53 +0300

    Обновил BurgerConstructor

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import { AppHeader } from "../app-header/app-header";
 import { BurgerIngredients } from "../burger-ingredients/burger-ingredients";
+import { BurgerConstructor } from "../burger-constructor/burger-constructor";
 
 import styles from "./app.module.css";
 
@@ -11,6 +12,7 @@ export const App = () => {
       <AppHeader />
       <main className={styles.main}>
         <BurgerIngredients />
+        <BurgerConstructor />
       </main>
     </div>
   );
