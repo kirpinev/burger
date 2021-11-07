413e8f50e0bcfb33a0d77c1de593f356677bceb1
Кирпинёв Игорь Валерьевич <IKirpinev@vimpelcom.ru>
2021-11-07 11:04:02 +0300

    BurgerIngredients

--- src/components/burger-ingredients/burger-ingredients.js
+++ src/components/burger-ingredients/burger-ingredients.js
@@ -1,4 +1,10 @@
-import React, { useState } from "react";
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 import _ from "lodash";
 import { Tab } from "@ya.praktikum/react-developer-burger-ui-components";
 
@@ -8,10 +14,54 @@ import { ingredients } from "../../mocks/data";
 import { ingredientTypes } from "../../constants/ingredient-type";
 import styles from "./burger-ingredients.module.css";
 
-const groupedIngredients = Object.entries(_.groupBy(ingredients, "type"));
-
 export const BurgerIngredients = () => {
-  const [current, setCurrent] = useState(ingredientTypes.bun);
+  const [currentIngredientType, setCurrentIngredientType] = useState(
+    ingredientTypes.bun
+  );
+
+  const bunRef = useRef();
+  const mainRef = useRef();
+  const sauceRef = useRef();
+
+  const groupedIngredients = useMemo(
+    () => Object.entries(_.groupBy(ingredients, "type")),
+    []
+  );
+  const [bunType, mainType, sauceType] = useMemo(
+    () => Object.keys(ingredientTypes),
+    []
+  );
+
+  const setRefForIngredientType = useCallback(
+    (name) => {
+      if (name === bunType) {
+        return bunRef;
+      }
+      if (name === mainType) {
+        return mainRef;
+      }
+      if (name === sauceType) {
+        return sauceRef;
+      }
+    },
+    [bunType, mainType, sauceType]
+  );
+
+  const scrollIntoIngredient = useCallback(() => {
+    if (currentIngredientType === ingredientTypes.bun) {
+      bunRef.current.scrollIntoView();
+    }
+    if (currentIngredientType === ingredientTypes.main) {
+      mainRef.current.scrollIntoView();
+    }
+    if (currentIngredientType === ingredientTypes.sauce) {
+      sauceRef.current.scrollIntoView();
+    }
+  }, [currentIngredientType]);
+
+  useEffect(() => {
+    scrollIntoIngredient();
+  }, [currentIngredientType, scrollIntoIngredient]);
 
   return (
     <section className={`${styles.section} pt-10`}>
@@ -19,31 +69,34 @@ export const BurgerIngredients = () => {
       <div className={`${styles.tabs} mb-10`}>
         <Tab
           value={ingredientTypes.bun}
-          active={current === ingredientTypes.bun}
-          onClick={setCurrent}
+          active={currentIngredientType === ingredientTypes.bun}
+          onClick={setCurrentIngredientType}
         >
           {ingredientTypes.bun}
         </Tab>
         <Tab
-          value={ingredientTypes.sauce}
-          active={current === ingredientTypes.sauce}
-          onClick={setCurrent}
-        >
-          {ingredientTypes.sauce}
-        </Tab>
-        <Tab
           value={ingredientTypes.main}
-          active={current === ingredientTypes.main}
-          onClick={setCurrent}
+          active={currentIngredientType === ingredientTypes.main}
+          onClick={setCurrentIngredientType}
         >
           {ingredientTypes.main}
+        </Tab>
+        <Tab
+          value={ingredientTypes.sauce}
+          active={currentIngredientType === ingredientTypes.sauce}
+          onClick={setCurrentIngredientType}
+        >
+          {ingredientTypes.sauce}
         </Tab>
       </div>
       <div className={`${styles.container} custom-scroll`}>
         <ul className={styles.ingredientsList}>
           {groupedIngredients.map(([type, ingredients]) => (
             <li className="mb-10" key={type}>
-              <h2 className="text text_type_main-medium mb-6">
+              <h2
+                ref={setRefForIngredientType(type)}
+                className="text text_type_main-medium mb-6"
+              >
                 {ingredientTypes[type]}
               </h2>
               <ul className={`${styles.ingredientsListByType}`}>
