45dc6f0e93e5d217b5c4593fcbd63aa9577e498d
Igor Kirpinev <kirpinev@yahoo.com>
2021-12-18 14:24:20 +0300

    Добавил страницу регистрации

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route } from "react-router-dom";
 
 import { MainPage } from "./pages/main-page/main-page";
+import { RegisterPage } from "./pages/register-page/register-page";
 
 import { appRoutes } from "./constants/app-routes";
 
@@ -8,6 +9,11 @@ export const App = () => {
   return (
     <BrowserRouter>
       <Route path={appRoutes.mainPage} exact={true} component={MainPage} />
+      <Route
+        path={appRoutes.registerPage}
+        exact={true}
+        component={RegisterPage}
+      />
     </BrowserRouter>
   );
 };
---
+++ src/pages/register-page/register-page.js
@@ -0,0 +1,37 @@
+import { AppHeader } from "components/app-header/app-header";
+import { Link } from "react-router-dom";
+
+import {
+  Input,
+  EmailInput,
+  PasswordInput,
+  Button,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+
+import styles from "global-styles/form.module.css";
+import { appRoutes } from "../../constants/app-routes";
+
+export const RegisterPage = () => {
+  return (
+    <>
+      <AppHeader />
+      <div className={styles.container}>
+        <h1 className="text text_type_main-medium">Регистрация</h1>
+        <Input placeholder="Имя" value="" onChange={() => {}} />
+        <EmailInput value="" name="Email" onChange={() => {}} />
+        <PasswordInput value="" name="Пароль" onChange={() => {}} />
+        <Button type="primary" size="medium">
+          Зарегистрироваться
+        </Button>
+        <p
+          className={`text text_type_main-default text_color_inactive ${styles.info}`}
+        >
+          Уже зарегистрированы?{" "}
+          <Link className={styles.link} to={appRoutes.loginPage}>
+            Войти
+          </Link>
+        </p>
+      </div>
+    </>
+  );
+};
