3910fb98b939d4cff465e6debb81bffb35bc9a4f
Igor Kirpinev <kirpinev@yahoo.com>
2021-12-24 12:43:50 +0300

    Добавил редирект на главную страницу по клику на лого, обновил компоненты

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,3 +1,5 @@
+import { useCallback } from "react";
+import { useHistory } from "react-router-dom";
 import {
   Logo,
   BurgerIcon,
@@ -10,43 +12,58 @@ import { appRoutes } from "constants/app-routes";
 
 import styles from "./app-header.module.css";
 
-export const AppHeader = () => (
-  <header className={`${styles.header} pt-4 pb-4`}>
-    <nav>
-      <ul className={styles.ul}>
-        <li>
-          <ul className={styles.ul}>
-            <li className="mr-2">
-              <HeaderNavLink
-                text="Конструктор"
-                Icon={BurgerIcon}
-                path={appRoutes.mainPage}
-                exact={true}
-              />
-            </li>
-            <li>
-              <HeaderNavLink
-                text="Лента заказов"
-                Icon={ListIcon}
-                path={appRoutes.profileOrders}
-                exact={false}
-              />
-            </li>
-          </ul>
-        </li>
-        <li className={styles.logo}>
-          <Logo />
-        </li>
-        <li>
-          <HeaderNavLink
-            text="Личный кабинет"
-            Icon={ProfileIcon}
-            path={appRoutes.profilePage}
-            matchPaths={[`${appRoutes.profilePage}${appRoutes.profileOrders}`]}
-            exact={false}
-          />
-        </li>
-      </ul>
-    </nav>
-  </header>
-);
+export const AppHeader = () => {
+  const history = useHistory();
+
+  const goToMainPage = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      history.push(appRoutes.mainPage);
+    },
+    [history]
+  );
+
+  return (
+    <header className={`${styles.header} pt-4 pb-4`}>
+      <nav>
+        <ul className={styles.ul}>
+          <li>
+            <ul className={styles.ul}>
+              <li className="mr-2">
+                <HeaderNavLink
+                  text="Конструктор"
+                  Icon={BurgerIcon}
+                  path={appRoutes.mainPage}
+                  exact={true}
+                />
+              </li>
+              <li>
+                <HeaderNavLink
+                  text="Лента заказов"
+                  Icon={ListIcon}
+                  path={appRoutes.profileOrders}
+                  exact={false}
+                />
+              </li>
+            </ul>
+          </li>
+          <li className={styles.logo} onClick={goToMainPage}>
+            <Logo />
+          </li>
+          <li>
+            <HeaderNavLink
+              text="Личный кабинет"
+              Icon={ProfileIcon}
+              path={appRoutes.profilePage}
+              matchPaths={[
+                `${appRoutes.profilePage}${appRoutes.profileOrders}`,
+              ]}
+              exact={false}
+            />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
--- src/components/profile-nav-link/profile-nav-link.js
+++ src/components/profile-nav-link/profile-nav-link.js
@@ -3,13 +3,13 @@ import PropTypes from "prop-types";
 
 import styles from "./profile-nav-link.module.css";
 
-export const ProfileNavLink = ({ path, text, onClick }) => (
+export const ProfileNavLink = ({ path, text, ...rest }) => (
   <NavLink
     className={`text text_type_main-medium text_color_inactive ${styles.navLink}`}
     exact={true}
     activeClassName={styles.navLinkActive}
     to={path}
-    onClick={onClick}
+    {...rest}
   >
     {text}
   </NavLink>
@@ -18,5 +18,4 @@ export const ProfileNavLink = ({ path, text, onClick }) => (
 ProfileNavLink.propTypes = {
   path: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
-  onClick: PropTypes.func.isRequired,
 };
