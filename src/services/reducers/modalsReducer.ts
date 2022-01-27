f146c1b75bc803743bef8ff5d1b81f3bc1075f88
Igor Kirpinev <kirpinev@yahoo.com>
2022-01-27 19:42:39 +0300

    Типизировал modals редьюсер

--- src/services/reducers/modalsReducer.ts
+++ src/services/reducers/modalsReducer.ts
@@ -4,13 +4,24 @@ import {
   TOGGLE_ERROR_ORDER_MODAL,
 } from "services/constants/modals";
 
-const initialState = {
+import { TModalsActions } from "services/actions/modals";
+
+type TModalsState = {
+  readonly isIngredientModalOpen: boolean;
+  readonly isErrorModalOpen: boolean;
+  readonly isSuccessOrderModalOpen: boolean;
+};
+
+const initialState: TModalsState = {
   isIngredientModalOpen: false,
   isErrorModalOpen: false,
   isSuccessOrderModalOpen: false,
 };
 
-export const modalsReducer = (state = initialState, action) => {
+export const modalsReducer = (
+  state = initialState,
+  action: TModalsActions
+): TModalsState => {
   switch (action.type) {
     case TOGGLE_INGREDIENT_MODAL:
       return {
