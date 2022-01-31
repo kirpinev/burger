4cdbc380c4ef0ea148a8fc3b3a26d1988ee5c8ff
Igor Kirpinev <kirpinev@yahoo.com>
2022-01-31 14:02:37 +0300

    Поменял мок файл ленты заказов на заказы из стора

--- src/components/feed-list/feed-list.tsx
+++ src/components/feed-list/feed-list.tsx
@@ -2,19 +2,27 @@ import { FC } from "react";
 
 import { FeedItem } from "components/feed-item/feed-item";
 
-import feedList from "mocks/feed-list.json";
+import { selectWsPublicFeedList } from "services/selectors/select-ws-public-feed";
+
+import { useSelector } from "hooks/use-selector";
+
+import { IFeedItem } from "types/feed-item";
 
 import styles from "./feed-list.module.css";
 
 export const FeedList: FC = (): JSX.Element => {
+  const feedList = useSelector(selectWsPublicFeedList);
+
   return (
     <section className={`${styles.section} pt-10`}>
       <h1 className="text text_type_main-large mb-5">Лента заказов</h1>
       <div className={`${styles.container} custom-scroll`}>
         <ul className={styles.feedList}>
-          {feedList.orders.map((order) => (
-            <FeedItem key={order.number} {...order} />
-          ))}
+          {feedList.map(
+            (order: IFeedItem): JSX.Element => (
+              <FeedItem key={order.number} {...order} />
+            )
+          )}
         </ul>
       </div>
     </section>
