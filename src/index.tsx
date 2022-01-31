9c88212953286a0057075592f3ac9601c104fa1e
Igor Kirpinev <kirpinev@yahoo.com>
2022-01-31 14:05:08 +0300

    Обновил импорты

--- src/index.tsx
+++ src/index.tsx
@@ -4,9 +4,9 @@ import { BrowserRouter } from "react-router-dom";
 import "@ya.praktikum/react-developer-burger-ui-components";
 
 import { App } from "components/app/app";
-import { ErrorBoundary } from "./components/error-boundary/error-boundary";
+import { ErrorBoundary } from "components/error-boundary/error-boundary";
 
-import { store } from "./services/store/store";
+import { store } from "services/store/store";
 
 import reportWebVitals from "./reportWebVitals";
 
