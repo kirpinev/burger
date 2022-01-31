131fd97243d229257a23e3780268178b2263ed89
Igor Kirpinev <kirpinev@yahoo.com>
2022-01-31 14:04:56 +0300

    Добавил типы экшенов для веб сокета

--- src/services/types/actions.ts
+++ src/services/types/actions.ts
@@ -3,8 +3,10 @@ import { TLoadingActions } from "services/actions/loading";
 import { TModalsActions } from "services/actions/modals";
 import { TOrderActions } from "services/actions/order";
 import { TUserActions } from "services/actions/user";
+import { TWSActions } from "services/actions/ws-public-feed";
 
 export type TApplicationActions =
+  | TWSActions
   | TIngredientsActions
   | TLoadingActions
   | TModalsActions
