c1b572e55f7f106396a694e9da01d1c7eb6dfb80
Igor Kirpinev <kirpinev@yahoo.com>
2022-02-08 19:38:13 +0300

    Добавил мок данные

---
+++ src/mocks/ingredient.ts
@@ -0,0 +1,63 @@
+import { IBurgerIngredient } from "types/burger-ingredient";
+
+export const ingredient: IBurgerIngredient = {
+  _id: "60d3b41abdacab0026a733c6",
+  name: "Краторная булка N-200i",
+  type: "bun",
+  proteins: 80,
+  fat: 24,
+  carbohydrates: 53,
+  calories: 420,
+  price: 1255,
+  image: "https://code.s3.yandex.net/react/code/bun-02.png",
+  image_mobile: "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
+  image_large: "https://code.s3.yandex.net/react/code/bun-02-large.png",
+  __v: 0,
+};
+
+export const ingredientsArray: ReadonlyArray<IBurgerIngredient> = [
+  {
+    _id: "60d3b41abdacab0026a733c6",
+    name: "Краторная булка N-200i",
+    type: "bun",
+    proteins: 80,
+    fat: 24,
+    carbohydrates: 53,
+    calories: 420,
+    price: 1255,
+    image: "https://code.s3.yandex.net/react/code/bun-02.png",
+    image_mobile: "https://code.s3.yandex.net/react/code/bun-02-mobile.png",
+    image_large: "https://code.s3.yandex.net/react/code/bun-02-large.png",
+    __v: 0,
+  },
+  {
+    _id: "60d3b41abdacab0026a733c7",
+    name: "Флюоресцентная булка R2-D3",
+    type: "bun",
+    proteins: 44,
+    fat: 26,
+    carbohydrates: 85,
+    calories: 643,
+    price: 988,
+    image: "https://code.s3.yandex.net/react/code/bun-01.png",
+    image_mobile: "https://code.s3.yandex.net/react/code/bun-01-mobile.png",
+    image_large: "https://code.s3.yandex.net/react/code/bun-01-large.png",
+    __v: 0,
+  },
+  {
+    calories: 4242,
+    carbohydrates: 242,
+    fat: 142,
+    image: "https://code.s3.yandex.net/react/code/meat-01.png",
+    image_large: "https://code.s3.yandex.net/react/code/meat-01-large.png",
+    image_mobile: "https://code.s3.yandex.net/react/code/meat-01-mobile.png",
+    name: "Биокотлета из марсианской Магнолии",
+    price: 424,
+    proteins: 420,
+    type: "main",
+    __v: 0,
+    _id: "60d3b41abdacab0026a733cb",
+  },
+];
+
+export const ingredientIndex = 1;
